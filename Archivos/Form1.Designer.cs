807ac39876190656a4b8a14b5a7d420165a12758
cerva <cerva@LORENACERVANTES>
2025-06-04 23:55:24 -0600

    Agregar archivos de proyecto.

---
+++ Archivos/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿using System;
+
+namespace Archivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.txtCarrera = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxArchivos = new System.Windows.Forms.ComboBox();
+            this.dataGridViewPersonas = new System.Windows.Forms.DataGridView();
+            this.btnAgregarDesdeArchivo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCargar.Font = new System.Drawing.Font("Microsoft YaHei UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCargar.Location = new System.Drawing.Point(12, 336);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(134, 74);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar Archivos";
+            this.btnCargar.UseVisualStyleBackColor = false;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(152, 336);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(113, 74);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(285, 336);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(117, 74);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(409, 436);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(104, 74);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnExportar.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportar.Location = new System.Drawing.Point(678, 396);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(109, 74);
+            this.btnExportar.TabIndex = 5;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = false;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtNombre.Location = new System.Drawing.Point(171, 127);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(208, 26);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtEdad.Location = new System.Drawing.Point(171, 189);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(208, 26);
+            this.txtEdad.TabIndex = 7;
+            // 
+            // txtCarrera
+            // 
+            this.txtCarrera.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtCarrera.Location = new System.Drawing.Point(171, 252);
+            this.txtCarrera.Name = "txtCarrera";
+            this.txtCarrera.Size = new System.Drawing.Size(208, 26);
+            this.txtCarrera.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SlateBlue;
+            this.label1.Location = new System.Drawing.Point(65, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.SlateBlue;
+            this.label2.Location = new System.Drawing.Point(65, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Edad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.SlateBlue;
+            this.label3.Location = new System.Drawing.Point(65, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Carrera";
+            // 
+            // comboBoxArchivos
+            // 
+            this.comboBoxArchivos.BackColor = System.Drawing.Color.DarkGray;
+            this.comboBoxArchivos.FormattingEnabled = true;
+            this.comboBoxArchivos.Location = new System.Drawing.Point(69, 32);
+            this.comboBoxArchivos.Name = "comboBoxArchivos";
+            this.comboBoxArchivos.Size = new System.Drawing.Size(146, 28);
+            this.comboBoxArchivos.TabIndex = 12;
+            // 
+            // dataGridViewPersonas
+            // 
+            this.dataGridViewPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPersonas.Location = new System.Drawing.Point(470, 99);
+            this.dataGridViewPersonas.Name = "dataGridViewPersonas";
+            this.dataGridViewPersonas.RowHeadersWidth = 62;
+            this.dataGridViewPersonas.RowTemplate.Height = 28;
+            this.dataGridViewPersonas.Size = new System.Drawing.Size(510, 263);
+            this.dataGridViewPersonas.TabIndex = 13;
+            // 
+            // btnAgregarDesdeArchivo
+            // 
+            this.btnAgregarDesdeArchivo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnAgregarDesdeArchivo.Font = new System.Drawing.Font("Microsoft YaHei", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregarDesdeArchivo.Location = new System.Drawing.Point(824, 396);
+            this.btnAgregarDesdeArchivo.Name = "btnAgregarDesdeArchivo";
+            this.btnAgregarDesdeArchivo.Size = new System.Drawing.Size(156, 74);
+            this.btnAgregarDesdeArchivo.TabIndex = 14;
+            this.btnAgregarDesdeArchivo.Text = "AgregarArchivo";
+            this.btnAgregarDesdeArchivo.UseVisualStyleBackColor = false;
+            this.btnAgregarDesdeArchivo.Click += new System.EventHandler(this.btnAgregarDesdeArchivo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1038, 539);
+            this.Controls.Add(this.btnAgregarDesdeArchivo);
+            this.Controls.Add(this.dataGridViewPersonas);
+            this.Controls.Add(this.comboBoxArchivos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCarrera);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "Form1";
+            this.Text = "Gestión de Archivos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void listViewPersona_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.TextBox txtCarrera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxArchivos;
+        private System.Windows.Forms.DataGridView dataGridViewPersonas;
+        private System.Windows.Forms.Button btnAgregarDesdeArchivo;
+    }
+}
+
+
---
+++ Archivos/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace Archivos
+{
+    public partial class Form1 : Form
+    {
+        string carpeta = @"C:\Users\cerva\source\repos\Archivos\Archivos\bin\Debug\Archivos";
+        string archivoActual = "";
+
+        List<Registro> lista = new List<Registro>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ConfigurarDataGridView();
+            CargarListaArchivos();
+            btnAgregarDesdeArchivo.Click += btnAgregarDesdeArchivo_Click;
+
+
+            txtNombre.KeyDown += TextBox_KeyDown;
+            txtEdad.KeyDown += TextBox_KeyDown;
+            txtCarrera.KeyDown += TextBox_KeyDown;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarListaArchivos();
+        }
+
+        void ConfigurarDataGridView()
+        {
+            dataGridViewPersonas.Columns.Clear();
+            dataGridViewPersonas.Columns.Add("Nombre", "Nombre");
+            dataGridViewPersonas.Columns.Add("Edad", "Edad");
+            dataGridViewPersonas.Columns.Add("Carrera", "Carrera");
+
+            dataGridViewPersonas.AllowUserToAddRows = false;
+            dataGridViewPersonas.ReadOnly = true;
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            CargarListaArchivos();
+            MessageBox.Show("Lista de archivos actualizada.");
+        }
+
+        void CargarListaArchivos()
+        {
+            comboBoxArchivos.Items.Clear();
+
+            if (!Directory.Exists(carpeta))
+            {
+                MessageBox.Show("La carpeta no existe.");
+                return;
+            }
+
+            var archivos = Directory.GetFiles(carpeta)
+                .Where(f => f.EndsWith(".json") || f.EndsWith(".txt") || f.EndsWith(".xlsx"));
+
+            foreach (string archivo in archivos)
+            {
+                string nombre = Path.GetFileName(archivo);
+                comboBoxArchivos.Items.Add(nombre);
+            }
+        }
+
+        private void comboBoxArchivos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxArchivos.SelectedItem == null) return;
+
+            string nombreArchivo = comboBoxArchivos.SelectedItem.ToString();
+            archivoActual = Path.Combine(carpeta, nombreArchivo);
+            lista.Clear();
+
+            if (archivoActual.EndsWith(".json"))
+                CargarDesdeJson();
+            else if (archivoActual.EndsWith(".txt"))
+                CargarDesdeTxt();
+            else if (archivoActual.EndsWith(".xlsx"))
+                CargarDesdeExcel();
+
+            MostrarPersonasEnDataGridView();
+        }
+
+        void CargarDesdeJson()
+        {
+            try
+            {
+                string texto = File.ReadAllText(archivoActual);
+                var opciones = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var datos = JsonSerializer.Deserialize<List<Registro>>(texto, opciones);
+
+                if (datos == null)
+                {
+                    MessageBox.Show("El archivo JSON no contiene datos válidos.");
+                    return;
+                }
+
+                lista = datos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al leer el archivo JSON:\n{ex.Message}");
+            }
+        }
+
+        void CargarDesdeTxt()
+        {
+            string[] lineas = File.ReadAllLines(archivoActual);
+            foreach (var linea in lineas)
+            {
+                string[] partes = linea.Split(',');
+                if (partes.Length == 3)
+                    lista.Add(new Registro(partes[0], int.Parse(partes[1]), partes[2]));
+            }
+        }
+
+        void CargarDesdeExcel()
+        {
+            var libro = new XLWorkbook(archivoActual);
+            var hoja = libro.Worksheet(1);
+            int fila = 2;
+
+            while (!hoja.Cell(fila, 1).IsEmpty())
+            {
+                string nombre = hoja.Cell(fila, 1).GetString();
+                int edad = int.Parse(hoja.Cell(fila, 2).GetString());
+                string carrera = hoja.Cell(fila, 3).GetString();
+                lista.Add(new Registro(nombre, edad, carrera));
+                fila++;
+            }
+        }
+
+        void MostrarPersonasEnDataGridView()
+        {
+            dataGridViewPersonas.Rows.Clear();
+
+            foreach (var reg in lista)
+            {
+                dataGridViewPersonas.Rows.Add(reg.Nombre, reg.Edad, reg.Carrera);
+            }
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            AgregarRegistro();
+        }
+
+        void AgregarRegistro()
+        {
+            string nombre = txtNombre.Text.Trim();
+            string edadTexto = txtEdad.Text.Trim();
+            string carrera = txtCarrera.Text.Trim();
+
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(edadTexto) || string.IsNullOrEmpty(carrera))
+            {
+                MessageBox.Show("Faltan datos.");
+                return;
+            }
+
+            if (!int.TryParse(edadTexto, out int edad))
+            {
+                MessageBox.Show("Edad inválida.");
+                return;
+            }
+
+            var nuevoRegistro = new Registro(nombre, edad, carrera);
+            lista.Add(nuevoRegistro);
+            dataGridViewPersonas.Rows.Add(nombre, edad, carrera);
+
+            MessageBox.Show("Registro agregado.");
+
+            txtNombre.Clear();
+            txtEdad.Clear();
+            txtCarrera.Clear();
+            txtNombre.Focus();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                AgregarRegistro();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string buscar = txtNombre.Text.ToLower();
+            var resultado = lista.FirstOrDefault(r => r.Nombre.ToLower().Contains(buscar));
+
+            if (resultado != null)
+                MessageBox.Show($"Encontrado: {resultado.Nombre}, {resultado.Edad}, {resultado.Carrera}");
+            else
+                MessageBox.Show("No encontrado.");
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(archivoActual))
+            {
+                MessageBox.Show("Seleccione un archivo.");
+                return;
+            }
+
+            if (archivoActual.EndsWith(".json"))
+            {
+                string json = JsonSerializer.Serialize(lista);
+                File.WriteAllText(archivoActual, json);
+            }
+            else if (archivoActual.EndsWith(".txt"))
+            {
+                var lineas = lista.Select(r => $"{r.Nombre},{r.Edad},{r.Carrera}");
+                File.WriteAllLines(archivoActual, lineas);
+            }
+            else if (archivoActual.EndsWith(".xlsx"))
+            {
+                var libro = new XLWorkbook();
+                var hoja = libro.Worksheets.Add("Datos");
+
+                hoja.Cell(1, 1).Value = "Nombre";
+                hoja.Cell(1, 2).Value = "Edad";
+                hoja.Cell(1, 3).Value = "Carrera";
+
+                int fila = 2;
+                foreach (var reg in lista)
+                {
+                    hoja.Cell(fila, 1).Value = reg.Nombre;
+                    hoja.Cell(fila, 2).Value = reg.Edad;
+                    hoja.Cell(fila, 3).Value = reg.Carrera;
+                    fila++;
+                }
+
+                libro.SaveAs(archivoActual);
+            }
+
+            MessageBox.Show("Archivo guardado.");
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(archivoActual))
+            {
+                MessageBox.Show("Seleccione un archivo.");
+                return;
+            }
+
+            if (archivoActual.EndsWith(".txt"))
+            {
+                MessageBox.Show("No se puede exportar un archivo de texto.");
+                return;
+            }
+
+            if (archivoActual.EndsWith(".json"))
+            {
+                string nuevo = archivoActual.Replace(".json", "_exportado.xlsx");
+                var libro = new XLWorkbook();
+                var hoja = libro.Worksheets.Add("Datos");
+
+                hoja.Cell(1, 1).Value = "Nombre";
+                hoja.Cell(1, 2).Value = "Edad";
+                hoja.Cell(1, 3).Value = "Carrera";
+
+                int fila = 2;
+                foreach (var reg in lista)
+                {
+                    hoja.Cell(fila, 1).Value = reg.Nombre;
+                    hoja.Cell(fila, 2).Value = reg.Edad;
+                    hoja.Cell(fila, 3).Value = reg.Carrera;
+                    fila++;
+                }
+
+                libro.SaveAs(nuevo);
+                MessageBox.Show("Exportado a Excel.");
+            }
+            else if (archivoActual.EndsWith(".xlsx"))
+            {
+                string nuevo = archivoActual.Replace(".xlsx", "_exportado.json");
+                string json = JsonSerializer.Serialize(lista);
+                File.WriteAllText(nuevo, json);
+                MessageBox.Show("Exportado a JSON.");
+            }
+        }
+
+        private void btnAgregarDesdeArchivo_Click(object sender, EventArgs e)
+        {
+            if (comboBoxArchivos.SelectedItem == null)
+            {
+                MessageBox.Show("Selecciona un archivo primero.");
+                return;
+            }
+             if (archivoActual.EndsWith(".json")) CargarDesdeJson();
+            else if (archivoActual.EndsWith(".txt")) CargarDesdeTxt();
+            else if (archivoActual.EndsWith(".xlsx")) CargarDesdeExcel();
+
+            // Toma el nombre del archivo seleccionado y construye la ruta completa
+            string carpeta = @"C:\Users\cerva\source\repos\Archivos\Archivos\bin\Debug\Archivos";
+            string nombreArchivo = comboBoxArchivos.SelectedItem.ToString();
+            archivoActual = Path.Combine(carpeta, nombreArchivo);
+
+            // Limpia la lista antes de agregar nuevos datos
+            lista.Clear();
+
+            // Carga según el tipo de archivo
+            if (archivoActual.EndsWith(".json"))
+                CargarDesdeJson();
+            else if (archivoActual.EndsWith(".txt"))
+                CargarDesdeTxt();
+            else if (archivoActual.EndsWith(".xlsx"))
+                CargarDesdeExcel();
+
+            // Muestra los registros en el DataGridView
+            MostrarPersonasEnDataGridView();
+            MessageBox.Show("Datos del archivo agregados a la lista.");
+        }
+    }
+
+    public class Registro
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Carrera { get; set; }
+
+        public Registro() { }
+
+        public Registro(string nombre, int edad, string carrera)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Carrera = carrera;
+        }
+    }
+}
